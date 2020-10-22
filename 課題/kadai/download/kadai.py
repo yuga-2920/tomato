f79addfe0b65f3fa98feb3ccb07ec3418abd874e
yuga-2920 <67497073+yuga-2920@users.noreply.github.com>
2020-10-22 14:57:23 +0900

    Update kadai.py

--- 課題/kadai/download/kadai.py
+++ 課題/kadai/download/kadai.py
@@ -5,53 +5,62 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 
 
-def predict(read_csv):
+def predict(test_x):
 
     df_x = pd.read_csv('train_x.csv')
     df_y = pd.read_csv('train_y.csv')
+    test_x = pd.read_csv("test_x.csv")
 
-    df_x1 = df_x.dropna(axis=1)
-    df_x1 = df_x1.loc[:, ~(df_x1.nunique() == 1)]
+    df = pd.merge(df_x, df_y, on="お仕事No.", how="outer")
+    y = df["応募数 合計"]
 
-    drop = ["お仕事No."]
-    for column in df_x1.columns:
-        if column not in read_csv.columns:
+    df = df.dropna(axis=1)
+    df = df.loc[:, ~(df.nunique() == 1)]
+
+    col = "給与/交通費　給与下限"
+    mean = df[col].mean()
+    sigma = df[col].mean()
+
+    low = mean - 3 * sigma
+    high = mean + 3 * sigma
+
+    df = df[(df[col] > low) & (df[col] < high)]
+
+    drop = ["お仕事No.", "応募数 合計"]
+    for column in df.columns:
+        if column not in test_x.columns:
             drop.append(column)
-        elif df_x1[column].dtype == object:
+        elif df[column].dtype == object:
             le = LabelEncoder()
-            le = le.fit(df_x1[column])
-            df_x1[column] = le.transform(df_x1[column])
+            le = le.fit(df[column])
+            df[column] = le.transform(df[column])
 
-    df_x1 = df_x1.drop(drop, axis=1)
+    df = df.drop(drop, axis=1)
 
-    X = df_x1
-    y = df_y["応募数 合計"]
-
-    X_array = np.array(X)
+    X_array = np.array(df)
     y_array = np.array(y)
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.4, random_state=1)
+        X_array, y_array, test_size=0.4, random_state=1)
 
     rfc = RandomForestRegressor(random_state=0)
     rfc.fit(X_train, y_train)
-    y_pred = rfc.predict(X_test)
 
     drop = ["お仕事No."]
-    for column in read_csv.columns:
-        if column not in X.columns:
+    for column in test_x.columns:
+        if column not in df.columns:
             drop.append(column)
-        elif read_csv[column].dtype == object:
+        elif test_x[column].dtype == object:
             le = LabelEncoder()
-            le = le.fit(read_csv[column])
-            read_csv[column] = le.transform(read_csv[column])
+            le = le.fit(test_x[column])
+            test_x[column] = le.transform(test_x[column])
 
-    No = read_csv["お仕事No."]
-    A1 = read_csv.drop(drop, axis=1)
+    No = test_x["お仕事No."]
+    A = test_x.drop(drop, axis=1)
 
-    A_array = np.array(A1)
-    pre_A_array = rfc.predict(A_array)
-
-    submit = pd.DataFrame({"お仕事No.": No, "応募数 合計": pre_A_array})
+    A_array = np.array(A)
+    A_pred = rfc.predict(A_array)
+    
+    submit = pd.DataFrame({"お仕事No.": No, "応募数 合計": A_pred})
 
     return submit
